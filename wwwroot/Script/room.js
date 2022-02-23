fe1ea6b98831a5f59bf49f9d833b384f71f07e94
TAN KOK KEONG <tankokkeong4488@gmail.com>
2022-02-23 16:33:11 +0800

    buy in code

--- wwwroot/Script/room.js
+++ wwwroot/Script/room.js
@@ -9,44 +9,64 @@ var cards =
 var playerSeat = [false, false, false, false, false];
 
 function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
   }
 
-  function cardDealing(){
+  return array;
+}
 
+function cardDealing(){
+
+}
+
+function buyInDisplay(){
+  var inputAmount = document.getElementById("buy-in-amount");
+  var inputManual = document.getElementById("buy-in-manual");
+  var displayAmount = document.getElementById("buy-in-amount-display");
+
+  if(inputAmount.value >= 0){
+      displayAmount.innerHTML = amountFormatter(inputAmount.value);
+      inputManual.value = inputAmount.value;
   }
+}
 
-  function buyInDisplay(){
-    var inputAmount = document.getElementById("buy-in-amount");
-    var displayAmount = document.getElementById("buy-in-amount-display");
+function buyInManual(){
+  var inputAmount = document.getElementById("buy-in-amount");
+  var inputManual = document.getElementById("buy-in-manual");
+  var displayAmount = document.getElementById("buy-in-amount-display");
+  var digit_validation = /^[0-9]+$/;
 
-    if(inputAmount.value >= 0){
-        displayAmount.innerHTML = amountFormatter(inputAmount.value);
-    }
+  if(!digit_validation.test(inputManual.value)){
+    inputManual.value =  inputManual.value.substring(0, inputManual.value.length-1);;
   }
-
-  function amountFormatter(amount){
-      var digitArray = amount.split("");
-      var arrayIndex = digitArray.length - 1;
-      var formattedAmount = "";
-
-      return Intl.NumberFormat('en-US').format(amount * 1);
+  else if(inputManual.value >= 1000000){
+    displayAmount.innerHTML = amountFormatter(1000000);
+    inputAmount.value = 1000000
+    inputManual.value = 1000000;
+    $("#buy-in-warning").html("");
   }
-
-  function buyInManual(){
-    var buyInAmount = document.getElementById("buy-in-manual");
+  else if(inputManual.value >= 100000){
+    displayAmount.innerHTML = amountFormatter(inputManual.value);
+    inputAmount.value = inputManual.value
+    inputManual.value = inputManual.value;
+    $("#buy-in-warning").html("");
   }
+  else{
+    $("#buy-in-warning").html("Your amount is less than the minimum amount!");
+  }
+}
+
+function amountFormatter(amount){
+  return Intl.NumberFormat('en-US').format(amount * 1);
+}
