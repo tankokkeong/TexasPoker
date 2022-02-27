a198abb7140dfce8da8ed65948f735bfc0c747de
Anson <zsho.anson11167@gmail.com>
2022-02-28 06:40:06 +0800

    latest progress

--- wwwroot/Script/miniGame.js
+++ wwwroot/Script/miniGame.js
@@ -28,6 +28,7 @@
         // Events
         // ========================================================================================
 
+        $('#leave').click( e => location = 'lobby.html');
 
 
         // ========================================================================================
@@ -41,6 +42,33 @@
         const conn = new signalR.HubConnectionBuilder()
             .withUrl('/minigameHub?' + param)
             .build();
+
+            conn.on('Reject', () => location = "lobby.html");
+
+            conn.on('Ready', (letter, game) => {
+                if (game.playerA){
+                    $('#nameA').text(game.playerA.name).show();
+                   
+                }
+    
+                if (game.playerB){
+                    $('#nameB').text(game.playerB.name).show();
+                    
+                }
+    
+                if(me == null){
+                    me = letter;
+                    $('#' + me).addClass('me');
+                }
+            });
+    
+            conn.on('Left', letter => {
+    
+                started = false;
+                $status.text(`Player ${letter} left. You win!`);
+                setTimeout(() => location = 'list.html', 3000);
+    
+            });
 
 
         //Start Connection
